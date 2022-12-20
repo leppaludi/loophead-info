29e3c43aed64cf6474243f06f3d1a9a4f6715a7d
Leppalúði <105874219+leppaludi@users.noreply.github.com>
2022-12-20 23:38:11 +0000

    OpenGraph

--- pages/[id]/[background]/[brain].tsx
+++ pages/[id]/[background]/[brain].tsx
@@ -8,8 +8,8 @@ import data from "../../../json/data.json";
 
 type Props = {
   head: typeof data.data[number];
-  brain: number
-  background: number
+  brain: number;
+  background: number;
 };
 
 export const getServerSideProps: GetServerSideProps<
@@ -23,10 +23,16 @@ export const getServerSideProps: GetServerSideProps<
   const { id, brain, background } = context.params!;
 
   const head = data.data.find((v) => v.name === id);
-  const backgroundNumber = Number(background)
-  const brainNumber = Number(brain)
+  const backgroundNumber = Number(background);
+  const brainNumber = Number(brain);
 
-  if (!head || backgroundNumber < 0 || backgroundNumber > 4 || brainNumber < 0 || brainNumber > 4) {
+  if (
+    !head ||
+    backgroundNumber < 0 ||
+    backgroundNumber > 4 ||
+    brainNumber < 0 ||
+    brainNumber > 4
+  ) {
     return {
       notFound: true,
     };
@@ -59,18 +65,18 @@ const ipfsImageToUrl = (img: string, background: number, brain: number) => {
 };
 
 const keyStr =
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
 const triplet = (e1: number, e2: number, e3: number) =>
   keyStr.charAt(e1 >> 2) +
   keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
   keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-  keyStr.charAt(e3 & 63)
+  keyStr.charAt(e3 & 63);
 
 const rgbDataURL = (r: number, g: number, b: number) =>
   `data:image/gif;base64,R0lGODlhAQABAPAA${
     triplet(0, r, g) + triplet(b, 255, 255)
-  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
 
 const Loophead = ({
   head,
@@ -87,6 +93,10 @@ const Loophead = ({
     <>
       <Head>
         <title>{title}</title>
+        <meta property="og:url" content="https://lh.lepp.io" />
+        <meta property="og:title" content={title} />
+        {url && <meta property="og:image" content={url} />}
+        <meta property="og:site_name" content="Loopheads" />
       </Head>
       <div className="text-center">
         <h1 className="text-3xl font-bold mb-3 mt-3">{title}</h1>
@@ -113,27 +123,81 @@ const Loophead = ({
         <div className="text-lg font-bold mb-2 mt-3">Background</div>
 
         <div className="inline-flex rounded-md shadow-sm">
-            <ButtonGroupLink active={background === 0} href={`/${head.name}/0/${brain}`} left>0</ButtonGroupLink>
-            <ButtonGroupLink active={background === 1} href={`/${head.name}/1/${brain}`}>1</ButtonGroupLink>
-            <ButtonGroupLink active={background === 2} href={`/${head.name}/2/${brain}`}>2</ButtonGroupLink>
-            <ButtonGroupLink active={background === 3} href={`/${head.name}/3/${brain}`}>3</ButtonGroupLink>
-            <ButtonGroupLink active={background === 4} href={`/${head.name}/4/${brain}`} right>4</ButtonGroupLink>
+          <ButtonGroupLink
+            active={background === 0}
+            href={`/${head.name}/0/${brain}`}
+            left
+          >
+            0
+          </ButtonGroupLink>
+          <ButtonGroupLink
+            active={background === 1}
+            href={`/${head.name}/1/${brain}`}
+          >
+            1
+          </ButtonGroupLink>
+          <ButtonGroupLink
+            active={background === 2}
+            href={`/${head.name}/2/${brain}`}
+          >
+            2
+          </ButtonGroupLink>
+          <ButtonGroupLink
+            active={background === 3}
+            href={`/${head.name}/3/${brain}`}
+          >
+            3
+          </ButtonGroupLink>
+          <ButtonGroupLink
+            active={background === 4}
+            href={`/${head.name}/4/${brain}`}
+            right
+          >
+            4
+          </ButtonGroupLink>
         </div>
 
         <div className="text-lg font-bold mb-2 mt-3">Brain</div>
 
         <div className="inline-flex rounded-md shadow-sm" role="group">
-            <ButtonGroupLink active={brain === 0} href={`/${head.name}/${background}/0`} left>0</ButtonGroupLink>
-            <ButtonGroupLink active={brain === 1} href={`/${head.name}/${background}/1`}>1</ButtonGroupLink>
-            <ButtonGroupLink active={brain === 2} href={`/${head.name}/${background}/2`}>2</ButtonGroupLink>
-            <ButtonGroupLink active={brain === 3} href={`/${head.name}/${background}/3`}>3</ButtonGroupLink>
-            <ButtonGroupLink active={brain === 4} href={`/${head.name}/${background}/4`} right>4</ButtonGroupLink>
+          <ButtonGroupLink
+            active={brain === 0}
+            href={`/${head.name}/${background}/0`}
+            left
+          >
+            0
+          </ButtonGroupLink>
+          <ButtonGroupLink
+            active={brain === 1}
+            href={`/${head.name}/${background}/1`}
+          >
+            1
+          </ButtonGroupLink>
+          <ButtonGroupLink
+            active={brain === 2}
+            href={`/${head.name}/${background}/2`}
+          >
+            2
+          </ButtonGroupLink>
+          <ButtonGroupLink
+            active={brain === 3}
+            href={`/${head.name}/${background}/3`}
+          >
+            3
+          </ButtonGroupLink>
+          <ButtonGroupLink
+            active={brain === 4}
+            href={`/${head.name}/${background}/4`}
+            right
+          >
+            4
+          </ButtonGroupLink>
         </div>
 
         <div className="text-lg font-bold mb-2 mt-3">Image URL</div>
 
         <div className="mt-3">
-          <a href={url || ''} className="text-blue-500 hover:text-blue-700">
+          <a href={url || ""} className="text-blue-500 hover:text-blue-700">
             {url}
           </a>
         </div>
